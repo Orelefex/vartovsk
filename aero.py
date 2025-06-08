fbfd5a1a635a21c9d99e759e98bc14d587c02263
Oleg Tkachenko <tkachenko.vodolei@yandex.ru>
2025-06-08 20:32:51 +0300

    Добавил страницы

--- aero.py
+++ aero.py
@@ -1,1 +1,4 @@
-a
+import streamlit as st
+
+st.title("Здесь будет аэрологическая диаграмма")
+st.write("Это только начало")
---
+++ metar.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Здесь будет таблица с metar")
+st.write("Это только начало")
