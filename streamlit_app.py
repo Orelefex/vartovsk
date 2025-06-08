484eac919c68abce1852c5d8a1f63fb78e170e02
Oleg Tkachenko <tkachenko.vodolei@yandex.ru>
2025-06-08 21:07:21 +0300

    Еще раз

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 aero_page = st.Page("aero.py", title="Аэрологическая диаграмма")
-metar_page = st.Page("metar.py", title="Metar")
+metar_page = st.Page("metar.py", title="METAR")
 
 pg = st.navigation([aero_page, metar_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
