7e719c4c6d1072e66be3c9b4de085f53b5c4e507
Oleg Tkachenko <tkachenko.vodolei@yandex.ru>
2025-06-08 20:29:12 +0300

    Two

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
+aero_page = st.Page("aero.py", title="Аэрологическая диаграмма")
+metar_page = st.Page("metar.py", title="Metar")
+
+pg = st.navigation([aero_page, metar_page])
+pg.run()
+
 st.title("My first app")
 st.write("Это только начало")
