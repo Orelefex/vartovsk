1b3c9ccea761a2b50e7fcdebc83e6bd0fbfa598e
Oleg Tkachenko <tkachenko.vodolei@yandex.ru>
2025-06-10 22:34:46 +0300

    Metar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # aero_page = st.Page("aero_app.py", title="Аэрологическая диаграмма")
 metar_page = st.Page("metar_app.py", title="METAR")
 
-pg = st.navigation([aero_page, metar_page])
+pg = st.navigation([metar_page])
 st.set_page_config(page_title="Помощник синоптика", page_icon=":material/edit:")
 pg.run()
 
